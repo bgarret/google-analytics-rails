34413f077cfcbe43eab31c74a61cde1db933a9a0
Benoit Garret <benoit.garret@wyvernplatform.com>
2011-07-05 14:23:03 +0200

    Tadaaaa! Helpers!

--- lib/google-analytics-rails.rb
+++ lib/google-analytics-rails.rb
@@ -1,1 +1,10 @@
+module GoogleAnalytics
+  module Rails
+    mattr_accessor :tracker
+    @@tracker = ""
+  end
+end
+
+GAR = GoogleAnalytics::Rails
+
 require 'google-analytics-rails/railtie' if defined?(Rails)
--- lib/google-analytics-rails/view_helpers.rb
+++ lib/google-analytics-rails/view_helpers.rb
@@ -1,5 +1,27 @@
+require 'google_analytics_tools'
+
 module GoogleAnalytics::Rails
   module ViewHelpers
+    def analytics_init
+      raise ArgumentError, "Tracker must be set! Run 'rake google-analytics:install' to create the initializer." if GAR.tracker.blank?
 
+      queue = GAQ.new
+      queue << GAQ::Events::SetAccount.new(GAR.tracker)
+      queue << GAQ::Events::TrackPageView.new
+      queue << GAQ::Events::TrackPageLoadTime.new
+
+      queue.to_s
+    end
+
+    def analytics_track_event(category, action, label, value)
+      analytics_render_event(GAQ::Events::TrackEvent.new(category, action, label, value))
+    end
+
+    private
+
+    def analytics_render_event(event)
+      raise ArgumentError, "Tracker must be set! Run 'rake google-analytics:install' to create the initializer." if GAR.tracker.blank?
+      GAQ::EventRenderer.new(event, GAR.tracker).to_s
+    end
   end
 end
