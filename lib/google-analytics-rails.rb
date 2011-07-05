bc4b4f434046683bf2d8279912935626130b538e
Benoit Garret <benoit.garret@wyvernplatform.com>
2011-07-05 10:52:12 +0200

    First commit

---
+++ lib/google-analytics-rails.rb
@@ -0,0 +1,7 @@
+module Google
+  module Analytics
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/google-analytics-rails/version.rb
@@ -0,0 +1,7 @@
+module Google
+  module Analytics
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
