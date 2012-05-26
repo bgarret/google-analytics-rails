00de124267c7c0727081fca4599129cff140a9e1
Benoit Garret <benoit.garret@gadz.org>
2012-05-26 12:29:13 +0200

    Test the analytics_init :anonymize option

--- test/rails/views_helper_test.rb
+++ test/rails/views_helper_test.rb
@@ -74,6 +74,24 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
     assert_equal(VALID_LOCAL_INIT, analytics_init(:local => true))
   end
 
+  VALID_INIT_WITH_ANONYMIZED_IP = <<-JAVASCRIPT
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount','TEST']);
+_gaq.push(['_gat._anonymizeIp']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+  JAVASCRIPT
+
+  def test_analytics_init_with_anonymized_ip
+    assert_equal(VALID_INIT_WITH_ANONYMIZED_IP, analytics_init(:anonymize => true))
+  end
+
   VALID_EVENT_INIT = <<-JAVASCRIPT
 <script type="text/javascript">
 var _gaq = _gaq || [];
